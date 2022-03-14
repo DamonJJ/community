197fa833f05f5618f3d034743f64a9ae9ca8b65b
DamonJJ <damon.jacoby@evereve.com>
2022-03-14 15:26:37 -0500

    Add private route

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,19 +7,21 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Private, Router, Route, Set } from '@redwoodjs/router'
 import EventsLayout from 'src/layouts/EventsLayout'
 import DashboardLayout from './layouts/DashboardLayout/DashboardLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={EventsLayout}>
-        <Route path="/events/new" page={EventNewEventPage} name="newEvent" />
-        <Route path="/events/{id:Int}/edit" page={EventEditEventPage} name="editEvent" />
-        <Route path="/events/{id:Int}" page={EventEventPage} name="event" />
-        <Route path="/events" page={EventEventsPage} name="events" />
-      </Set>
+      <Private unauthenticated="home">
+        <Set wrap={EventsLayout}>
+          <Route path="/admin/events/new" page={EventNewEventPage} name="newEvent" />
+          <Route path="/admin/events/{id:Int}/edit" page={EventEditEventPage} name="editEvent" />
+          <Route path="/admin/events/{id:Int}" page={EventEventPage} name="event" />
+          <Route path="/admin/events" page={EventEventsPage} name="events" />
+        </Set>
+      </Private>
       <Set wrap={DashboardLayout}>
         <Route path="/get-together/{id:Int}" page={GetTogetherPage} name="getTogether" />
         <Route path="/contact" page={ContactPage} name="contact" />
